4187960cdc2a7c3be81516ae1c570ea3d89c7c48
Leonardo Casales <leonardo@smartnet.com.ar>
2019-08-04 21:22:39 -0300

    test con makefile

---
+++ stack/src/stackTest.cpp
@@ -0,0 +1,36 @@
+#include "iStack.h"
+#include "Stack.cpp"
+#include "testHeader.cpp"
+
+
+// g++ -std=c++11 -Wall stackTest.cpp && ./stackTest --reporter compact --success
+
+SCENARIO( "Stack puede apilar", "" ) {
+    GIVEN( "Una instancia de Stack" ) {
+        iStack* s = new Stack( 10 ) ;
+        WHEN( "Apilo un 5") {
+            s->push( 5 );
+            Type item = s->pop() ;
+            THEN( "debería obtener un 5" ) {
+                REQUIRE( item == 5 ) ;
+            }
+        }
+        WHEN( "Apilo un 9") {
+            s->push( 9 );
+            Type item = s->pop() ;
+            THEN( "debería obtener un 9" ) {
+                REQUIRE( item == 9 ) ;
+            }
+        }
+        WHEN( "Apilo un 5 y un 9") {
+            s->push( 5 );
+            s->push( 9 );
+            Type item1 = s->pop() ;
+            Type item2 = s->pop() ;
+            THEN( "debería obtener un 9 y un 5" ) {
+                REQUIRE( item1 == 9 ) ;
+                REQUIRE( item1 == 5 ) ;
+            }
+        }
+    }
+}
---
+++ stack/src/testHeader.cpp
@@ -0,0 +1,13 @@
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_FAST_COMPILE
+// "BDD-style" convenience wrappers
+// #define SCENARIO( ... ) TEST_CASE( "Scenario: " __VA_ARGS__ )
+// #define SCENARIO_METHOD( className, ... ) INTERNAL_CATCH_TEST_CASE_METHOD( className, "Scenario: " __VA_ARGS__ )
+
+// #define GIVEN( desc )    SECTION( std::string("   Given: ") + desc )
+// #define WHEN( desc )     SECTION( std::string("    When: ") + desc )
+// #define AND_WHEN( desc ) SECTION( std::string("And when: ") + desc )
+// #define THEN( desc )     SECTION( std::string("    Then: ") + desc )
+// #define AND_THEN( desc ) SECTION( std::string("     And: ") + desc )
+
+#include "../catch2/catch.hpp"
