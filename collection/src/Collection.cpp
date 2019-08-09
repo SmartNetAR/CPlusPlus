2775e925f58b4ec38f05f7b5030acc1c8d8e4c70
Leonardo Casales <leonardo@smartnet.com.ar>
2019-08-09 11:37:39 -0300

    collection created + microiteration 0,3

---
+++ collection/src/Collection.cpp
@@ -0,0 +1,21 @@
+#ifndef Collection_cpp
+#define Collection_cpp
+
+#include <iostream>
+
+struct Word {
+  bool isNull( void ) {
+    bool result = true ;
+    return result ;
+  }
+} ;
+void process ( Word thisWord ) {
+
+}
+
+Word getWordFromText( void ) {
+  Word word ;
+
+}
+
+#endif
---
+++ collection/src/test/collectionTest.cpp
@@ -0,0 +1,70 @@
+#include "../Collection.cpp"
+#include "catch2/catch.hpp"
+/* Dado un texto que termina en un punto. Construir una lista de palabras
+    y su frecuencia ( veces que palabra aparece en el texto ).
+    El texto se procesa caracter a caracter. */
+
+SCENARIO( "Collection puede agregar palabras", "" ) {
+    GIVEN( "Mientras haya una palabra en el texto obtenerla y procesarla" ) {
+        Word word ;
+        WHEN( "Agrego solo un 5, la propia función devuelve el item") {
+            word = getWordFromText() ;
+            // int item = l.append( 5 );
+            // Type item = l->pop() ;
+            THEN( "debería obtener un 5" ) {
+                while( !( word.isNull() )) {
+                    process( word ) ;
+                    word = getWordFromText() ;
+                }
+                // REQUIRE( item == 5 ) ;
+            }
+        }
+        /*
+        WHEN( "Encolo solo un 9") {
+            l->append( 9 );
+            Type item = l->pop() ;
+            THEN( "debería obtener un 9" ) {
+                REQUIRE( item == 9 ) ;
+            }
+        } */
+        /* WHEN( "Encolo un 5, un 9 y un 13") {
+            l->append( 5 );
+            l->append( 9 );
+            l->append( 13 );
+            Type item1 = l->pop() ;
+            Type item2 = l->pop() ;
+            Type item3 = l->pop() ;
+            THEN( "debería obtener un 5, un 9 y un 13" ) {
+                REQUIRE( item1 == 5 ) ;
+                REQUIRE( item2 == 9 ) ;
+                REQUIRE( item3 == 13 ) ;
+            }
+        }
+        WHEN( "Encolo un 6 y un 7 y consulto el último dos veces") {
+            l->append( 6 ) ;
+            l->append( 7 ) ;
+            auto item1 = l->peekBack() ;
+            auto item2 = l->peekBack() ;
+            THEN( "debería obtener un 7 ambas veces" ) {
+                REQUIRE( item1 == 7 ) ;
+                REQUIRE( item2 == 7 ) ;
+            }
+        }
+        WHEN( "Encolo un 6 y un 7 y consulto el primero dos veces") {
+            l->append( 6 ) ;
+            l->append( 7 ) ;
+            auto item1 = l->peekFront() ;
+            auto item2 = l->peekFront() ;
+            THEN( "debería obtener un 6 ambas veces" ) {
+                REQUIRE( item1 == 6 ) ;
+                REQUIRE( item2 == 6 ) ;
+            }
+        }
+        WHEN ( "Está vacía y quiero desencolar" ) {
+            // auto item = l->pop() ;
+            THEN( "qué debería obtener un error o un nullptr" ) {
+                // REQUIRE( item == ? ) ;
+            }
+        } */
+    }
+}
