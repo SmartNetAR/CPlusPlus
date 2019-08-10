1306b40e93b33d56e77371970b262c97a2510eac
Leonardo Casales <leonardo@smartnet.com.ar>
2019-08-10 19:12:07 -0300

    contar palabras integrador v1

--- 03emb Ahorcado/Android/Debug/Principal.d
+++
@@ -1,4 +0,0 @@
-.\Android\Debug\Principal.o: Principal.cpp \
-  C:\Temp\GIT\CPlusPlus\Embarcadero\Ahorcado/Principal.h \
-  C:\Temp\GIT\CPlusPlus\Embarcadero\Ahorcado/Ahorcado.h Principal.fmx \
-  Principal.LgXhdpiPh.fmx
